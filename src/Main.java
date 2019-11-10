0a888556c5ac0ce4e0580e3dc5f416fb536c66a6
ohoff <ohoffman93@gmail.com>
2019-11-10 13:59:46 +0200

    6 commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello omer!");
+
+        System.out.println("hello again");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("is it working?");
+
+        }
     }
 }

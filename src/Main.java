15eea048defc7dee0147d7333197740aa50293f0
ohoff <ohoffman93@gmail.com>
2019-11-10 14:05:38 +0200

    6 commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         System.out.println("hello again");
         for (int i = 0; i < 10; i++) {
-            System.out.println("is it working?");
+            System.out.println("is it worddking?");
 
         }
     }

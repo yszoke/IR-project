ce992c47c8a3bf9f2d10e4c5f4f029cee008aa15
yszoke <szoke@post.bgu.ac.il>
2019-12-10 10:46:34 +0200

    read file

--- src/ReadFile/ReadFileJsoup.java
+++ src/ReadFile/ReadFileJsoup.java
@@ -34,7 +34,6 @@ public class ReadFileJsoup implements ReadFileMethods {
     }
 
 
-
     /**
      * this function takes the files from the folder and split them docs, then send only the text part to parse class
      *

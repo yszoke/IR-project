c3687e0a1f8217dceec9d28863ff7e560ff66c50
yszoke <szoke@post.bgu.ac.il>
2019-12-12 17:40:43 +0200

    dictionary

--- src/invertedIndex/Dictionary.java
+++ src/invertedIndex/Dictionary.java
@@ -10,6 +10,8 @@ public class Dictionary {
 
     File file;
     private HashMap<String, String> dictionary;
+    private HashMap<String,Integer> wordsInDoc;
+    private HashMap<String,Integer> popularWordInDoc;
 
     /**
      * constructor
@@ -19,7 +21,17 @@ public class Dictionary {
     public Dictionary(File file) {
         this.file = file;
         dictionary = new HashMap<>();
+        wordsInDoc = new HashMap<>();
+        popularWordInDoc = new HashMap<>();
+        //init();
     }
+
+//    private void init() {
+//        for(int i=1; i<=472525; i++){
+//            wordsInDoc.put(String.valueOf(i),0);
+//            popularWordInDoc.put(String.valueOf(i),0);
+//        }
+//    }
 
     /**
      * this function reads the file, split to x posting files and creates the dictionary
@@ -28,14 +40,18 @@ public class Dictionary {
      */
     public void create() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(file));
+
         String[] line;
+        int index = 0;
         int counterFile = 0;
         int counterLine = 0;
         int pointerLine = 1;
         int Ndocs = 0;
+        int shows = 0;
         String previousWord = "";
         String currentWord = "";
         String ln = reader.readLine();
+        String doc = "";
         // while the line we read isn't null
         while (ln != null) {
             // if the line is a space, we skip
@@ -50,7 +66,24 @@ public class Dictionary {
             // each x lines is one posting file
             while (ln != null && (counterLine < 100000)) {
                 line = ln.split(" ");
+                if (line.length < 3) {
+                    continue;
+                }
                 currentWord = calculateWord(line);
+                doc = calculateDoc(line);
+                if (wordsInDoc.containsKey(doc)) {
+                    wordsInDoc.put(doc, wordsInDoc.get(doc) + 1);
+                } else {
+                    wordsInDoc.put(doc, 1);
+                }
+                shows = calculateShows(line);
+                if (popularWordInDoc.containsKey(doc)) {
+                    if (shows > popularWordInDoc.get(doc)) {
+                        popularWordInDoc.put(doc, shows);
+                    }
+                } else {
+                    popularWordInDoc.put(doc, shows);
+                }
                 if (currentWord.equals(previousWord)) {
                     Ndocs++;
                 } else {
@@ -67,7 +100,31 @@ public class Dictionary {
             }
             pw.close();
             ln = reader.readLine();
+            System.out.println(index);
+            index++;
         }
+    }
+
+    /**
+     *
+     * @param line
+     * @return
+     */
+    private int calculateShows(String[] line) {
+        String list=line[line.length-1];
+        String [] locations= list.split(",");
+        return locations.length;
+    }
+    /**
+     *
+     * @param line
+     * @return
+     */
+    private String calculateDoc(String[] line) {
+        if(line.length>3){
+            return line[line.length-2];
+        }
+        return line[1];
     }
 
     /**
